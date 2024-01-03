eaab865f6fa2d88e110bf0a3660df925ae5240f2
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2024-01-04 00:28:07 +0800

    Add etching and grid size / reset functionality

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 
 const gridContainer = document.getElementById("gridContainer");
-let gridSize = 100;
+const gridPoint = document.getElementsByClassName("gridPoint");
+const resizeButton = document.getElementById("resizeButton");
+let gridSize = 16;
 let gridWidth = (window.innerWidth / 2);
 let pointSize = (gridWidth / gridSize);
-const gridPoint = document.getElementById("gridPoint");
 
+drawGrid();
 
+resizeButton.addEventListener("click", function() {
+    gridSize = prompt("Please enter grid division (max: 100)", 16);
+    if (gridSize > 100) {
+        gridSize = 100;
+    }
+    gridWidth = (window.innerWidth / 2);
+    pointSize = (gridWidth / gridSize);
+    clearGrid();
+    drawGrid();
+});
 
-
+function drawGrid() {
 for (let step = 0; step < (gridSize ** 2); step++) {
-const gridPoint = document.createElement("div");
-gridPoint.id = "gridPoint";
-gridPoint.style.width = pointSize + "px";
-gridPoint.style.height = pointSize + "px";
-document.getElementById("gridContainer").appendChild(gridPoint);
-console.log('x');
+gridContainer.style.width = gridWidth + "px";
+gridContainer.style.height = gridWidth + "px";
+const point = document.createElement("div");
+point.classList.add("gridPoint");
+point.style.width = pointSize + "px";
+point.style.height = pointSize + "px";
+point.addEventListener("mouseover", (event) => {
+    //point.classList.add("etchedPoint");
+    point.style.backgroundColor = "purple";
+});
+gridContainer.appendChild(point);
+
+//console.log('x');
+}
 }
 
+function clearGrid() {
+const trash = document.getElementById("gridContainer");
+while(trash.firstChild) {
+    trash.removeChild(trash.firstChild);
+}
+}
